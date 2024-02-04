3a6965dadd8f55d79fb02d320633244bbb6cbe99
tariqul-dev <tariqulislam.dev@gmail.com>
2024-02-04 12:40:27 +0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Bdjobs Mac Mini on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
